d883125f34981f4f72a52892215948fd862f2c4f
pratik.mondal <pmondal2424@gmail.com>
2021-12-16 13:16:38 +0600

    Inheritance

--- src/com/company/concepts/oop/inheritance/Student.java
+++ src/com/company/concepts/oop/inheritance/Student.java
@@ -1,4 +1,38 @@
 package com.company.concepts.oop.inheritance;
 
-public class Student {
+public class Student extends Person{
+
+    private String universityName;
+    private String studentId;
+
+
+    Student(){
+
+    }
+
+    Student(String name, String age, String uni){
+
+    }
+
+    void getPersonDetails(){
+        System.out.println("Name: "+name);
+        System.out.println("Age " +age);
+        System.out.println("University "+universityName);
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
 }
