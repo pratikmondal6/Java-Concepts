44a852724e5fc7397f1f10d70cecc36ecf50dd43
pratik.mondal <pmondal2424@gmail.com>
2021-12-16 20:14:06 +0600

    Interface

---
+++ src/com/company/concepts/oop/abstraction/abstractInterface/Animal.java
@@ -0,0 +1,7 @@
+package com.company.concepts.oop.abstraction.abstractInterface;
+
+public interface Animal {
+
+    void Eat();
+
+}
---
+++ src/com/company/concepts/oop/abstraction/abstractInterface/Dog.java
@@ -0,0 +1,9 @@
+package com.company.concepts.oop.abstraction.abstractInterface;
+
+public class Dog implements Animal{
+
+    @Override
+    public void Eat() {
+        System.out.println("Dog eat meat");
+    }
+}
